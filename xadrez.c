5db23d07a52ba088b7bf6b99bb3ff803f2c8f5fc
Roberth Silva <roberthlucas898@gmail.com>
2025-09-23 15:52:19 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
 int main(){
-
-    // programação da torre
     int movimento_torre=1;
     int movimento_bispo=1;
 
+// programação da torre
     printf("Torre está se movimentando\n");
     while (movimento_torre <=5)
     {
@@ -19,23 +14,37 @@ int main(){
 
     printf("A torre terminou seu movimento \n\n");
 
-        // programação do bispo
-        printf("Bispo está se movimentando\n");
+// programação do bispo
+    printf("Bispo está se movimentando\n");
         do
         {
             printf("Cima, direita\n");
             movimento_bispo++;
         } while (movimento_bispo<=5);
 
-        printf("O bispo terminou seu movimento \n\n");
+    printf("O bispo terminou seu movimento \n\n");
 
-            //programação rainha
-            printf("A rainha está movimentando\n");
-            for (int movimento_rainha = 0; movimento_rainha <= 7; movimento_rainha++)
-            {
-                printf("esquerda\n");
-            }
-            printf("A rainha terminou seu movimento \n\n");
+//programação rainha
+    printf("A rainha está movimentando\n");
+        for (int movimento_rainha = 0; movimento_rainha <= 7; movimento_rainha++)
+        {
+            printf("esquerda\n");
+        }
+        printf("A rainha terminou seu movimento \n\n");
+
+// programação do cavalo
+    for (int movimento_cavalo = 0; movimento_cavalo <= 1; movimento_cavalo++)
+    {
+        int movimento_cavalo2=1;
+        while (movimento_cavalo2<=2)
+        {
+            printf("Baixo\n");
+            ++movimento_cavalo2;
+        }
+        printf("Esquerda\n\n");
+        ++movimento_cavalo;
+    }
+
 
     return 0;
 }
