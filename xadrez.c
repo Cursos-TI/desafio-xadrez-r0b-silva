d37f593edcab6586739056e155240f1aa0e6b8f3
Roberth Silva <roberthlucas898@gmail.com>
2025-09-23 17:40:20 -0300

    Update xadrez.c
    atualização do nível mestre

--- xadrez.c
+++ xadrez.c
@@ -1,49 +1,74 @@
 #include <stdio.h>
 
-int main(){
-    int movimento_torre=1;
-    int movimento_bispo=1;
-
-// programação da torre
-    printf("Torre está se movimentando\n");
-    while (movimento_torre <=5)
+void movimento_torre(int torre){
+    if (torre < 5)
     {
-        printf("Direita\n");
-        movimento_torre++;
+       printf("direita\n");
+       movimento_torre(torre + 1);
+    }
+}
+
+void movimento_rainha(int rainha){
+    if (rainha < 8)
+    {
+        printf("Esquerda\n");
+        movimento_rainha(rainha + 1);
+    }
+    
+}
+
+
+void mover_bispo(int cima, int direita, int limite) {
+    if (cima >= limite || direita >= limite) {
+        printf("O bispo terminou seu movimento\n\n");
+        return;
     }
 
+    for (int i = cima; i < limite; i++) {
+        printf("cima\n");
+            for (int j = direita; j < limite; j++) {
+                printf("direita\n");
+
+                mover_bispo(i + 1, j + 1, limite);
+                return;
+            }
+    }
+}
+
+
+int main(){
+// movimento torre
+    printf("A torre está movimentando\n");
+    movimento_torre(0);
     printf("A torre terminou seu movimento \n\n");
 
 // programação do bispo
-    printf("Bispo está se movimentando\n");
-        do
-        {
-            printf("Cima, direita\n");
-            movimento_bispo++;
-        } while (movimento_bispo<=5);
+    printf("O bispo está movimentando\n");
 
-    printf("O bispo terminou seu movimento \n\n");
+    
+printf("O bispo está movimentando\n\n");
+    mover_bispo(0, 0, 5); // Simula até a posição (3,3)
+    
 
 //programação rainha
     printf("A rainha está movimentando\n");
-        for (int movimento_rainha = 0; movimento_rainha <= 7; movimento_rainha++)
-        {
-            printf("esquerda\n");
-        }
+        movimento_rainha(0);
         printf("A rainha terminou seu movimento \n\n");
 
 // programação do cavalo
+printf("O cavalo está movimentando\n");
     for (int movimento_cavalo = 0; movimento_cavalo <= 1; movimento_cavalo++)
     {
         int movimento_cavalo2=1;
         while (movimento_cavalo2<=2)
         {
-            printf("Baixo\n");
+            printf("cima\n");
             ++movimento_cavalo2;
         }
-        printf("Esquerda\n\n");
+        printf("direita\n");
         ++movimento_cavalo;
     }
+    printf("o cavalo terminou seu movimento \n\n");
 
 
     return 0;
